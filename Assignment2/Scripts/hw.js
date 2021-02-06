9b9da6469d114c916e502ed982151d1c7f0890e0
deepen1987 <deepen1987@gmail.com>
2021-02-06 14:32:15 -0500

    HW

--- Assignment2/Scripts/hw.js
+++ Assignment2/Scripts/hw.js
@@ -20,3 +20,24 @@ function random() {
   var num2 = Math.floor(Math.random() * 10000);
   document.getElementById("random").textContent = num2;
 }
+
+function guess() {
+  var num3 = Math.floor(Math.random() * 10);
+  var num4 = document.getElementById("input1").value;
+  if (sessionStorage.clickcount) {
+    sessionStorage.clickcount = Number(sessionStorage.clickcount) + 1;
+  } else {
+    sessionStorage.clickcount = 0;
+  }
+  if (sessionStorage.clickcount < 3) {
+    if (num4 == num3) {
+      //   document.getElementById("debug").textContent = sessionStorage.clickcount; Used them for debugging
+      alert("Congratulations your guess is correct");
+    } else if (num4 != num3) {
+      //   document.getElementById("debug").textContent = sessionStorage.clickcount; Used them for debugging
+      alert("You have guessed incorrect number correct number is " + num3);
+    }
+  } else {
+    alert("Maximum 3 guess per session");
+  }
+}
