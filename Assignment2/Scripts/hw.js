51bcb095f31132971ec12711190805a9778e1458
deepen1987 <deepen1987@gmail.com>
2021-02-07 08:28:53 -0500

    HW2

--- Assignment2/Scripts/hw.js
+++ Assignment2/Scripts/hw.js
@@ -3,7 +3,7 @@
     JavaScript HW-2*/
 
 function isBetween() {
-  var num1 = document.getElementById("us1").value;
+  var num1 = document.getElementById("num").value;
   if ((num1 < 18 || num1 > 24) && num1 != -1) {
     alert("Your entered number " + num1 + " is not between 18 and 24");
   } else if (num1 == -1) {
@@ -15,29 +15,74 @@ function isBetween() {
   }
 }
 
-function random() {
+function randomGen() {
   //math.random generates a number between 0 - 1 and mat.floor generates a largest integer less than or equal to specified number
-  var num2 = Math.floor(Math.random() * 10000);
-  document.getElementById("random").textContent = num2;
+  var random_gen = Math.floor(Math.random() * 10000);
+  document.getElementById("random").textContent = random_gen;
 }
 
-function guess() {
-  var num3 = Math.floor(Math.random() * 10);
-  var num4 = document.getElementById("input1").value;
+function guessRandom() {
+  var random_number = Math.floor(Math.random() * 10);
+  var user_num = document.getElementById("guess_num").value;
   if (sessionStorage.clickcount) {
     sessionStorage.clickcount = Number(sessionStorage.clickcount) + 1;
   } else {
     sessionStorage.clickcount = 0;
   }
   if (sessionStorage.clickcount < 3) {
-    if (num4 == num3) {
+    if (user_num == random_number) {
       //   document.getElementById("debug").textContent = sessionStorage.clickcount; Used them for debugging
       alert("Congratulations your guess is correct");
-    } else if (num4 != num3) {
+    } else if (user_num != random_number) {
       //   document.getElementById("debug").textContent = sessionStorage.clickcount; Used them for debugging
-      alert("You have guessed incorrect number correct number is " + num3);
+      alert(
+        "You have guessed incorrect number correct number is " + random_number
+      );
     }
   } else {
     alert("Maximum 3 guess per session");
+  }
+}
+
+function ageVerify() {
+  var today = new Date();
+  var today_dd = today.getDate();
+  var today_mm = today.getMonth() + 1;
+  var today_yyyy = today.getFullYear();
+
+  var birth_date = document.getElementById("birth_date").value;
+  const x = birth_date.split("-");
+  var birth_year = Number(x[0]);
+  var birth_month = Number(x[1]);
+  var birth_day = Number(x[2]);
+
+  if (
+    (birth_year == today_yyyy && birth_month > today_mm) ||
+    birth_date > today_dd
+  ) {
+    alert("Birth date cannot be greater than today's date.");
+  } else if (birth_year > today_yyyy) {
+    alert("Birth date cannot be greater than today's date.");
+  }
+
+  var age = today_yyyy - birth_year;
+  var months = today_mm - birth_month;
+
+  if (months < 0 || (months == 0 && today_dd < birth_day)) {
+    age--;
+  }
+
+  if (age == 21 && today_dd == birth_day && months == 0) {
+    alert("Happy Birthday! You are 21 now...");
+  } else if (age == 20 && months == 0 && today_dd < birth_day) {
+    alert("Your 21st birthday is only few days away");
+  } else if (age == 20 && months < 0) {
+    alert("Your 21st birthday is only few months away");
+  } else if (age == 20) {
+    alert("Your 21st birthday is coming up next year");
+  } else if (age > 21) {
+    alert("You are over 21.");
+  } else {
+    alert("You are not 21 yet...");
   }
 }
--- Practice/Scripts/trialsite.js
+++ Practice/Scripts/trialsite.js
@@ -1,23 +1,10 @@
-function cars(car_brand, car_model, car_price) {
-  this.car_brand = car_brand;
-  this.car_model = car_model;
-  this.car_price = car_price;
+function user_pass() {
+  var a = document.getElementById("username").value;
+  var b = document.getElementById("password").value;
 
-  this.testautopilot = function () {
-    return "this is an autopilot \n";
-  };
-}
-
-var c1 = new cars("tesla", 3, 35000);
-var c2 = new cars("tesla", "S", 65000);
-
-document.write(c1.testautopilot());
-document.write(c1.car_model + "\n");
-document.write(c1.car_price + "\n");
-document.write(c2.car_model + "\n");
-document.write(c2.car_price) + "\n";
-
-function changeText() {
-  var x = (document.getElementById("p").innerHTML = "Deependra was here");
-  alert(x);
+  if (a == b) {
+    alert("You are being logged in username and password match");
+  } else {
+    alert("Please enter correct username and password.");
+  }
 }
