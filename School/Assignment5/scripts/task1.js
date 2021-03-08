c94eb19d1adcfdb008677a8856f683677e2278bb
Deependrasingh Shekhawat <deepen1987@gmail.com>
2021-03-08 17:16:30 -0500

    commit

--- School/Assignment5/scripts/task1.js
+++ School/Assignment5/scripts/task1.js
@@ -1,26 +1,8 @@
 $(document).ready(function () {
-  let index = 1;
   function sleep() {
-    setTimeout(() => {
-      console.log(index);
+    for (let index = 0; index < 10; index++) {
       $(".content").text(index);
-      index++;
-      if (index <= 10) {
-        sleep();
-      }
-    }, 1000);
+    }
   }
-
   sleep();
-
-  //   const wait = (ms) => new Promise((res) => setTimeout(res, ms));
-
-  //   async function load() {
-  //     for (let index = 0; index < 10; index++) {
-  //       await wait(1000);
-  //       $(".content").text(index);
-  //     }
-  //   }
-
-  //   load();
 });
