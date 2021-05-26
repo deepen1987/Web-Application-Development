efb35185b4cbc19ecb67925afe3f75387ea4b5da
Deependrasingh Shekhawat <deepen1987@gmail.com>
2021-05-26 15:06:21 -0400

    commit

--- Learning/LearningFront/control-flow/11-primeNumber.js
+++ Learning/LearningFront/control-flow/11-primeNumber.js
@@ -1,15 +1,13 @@
-
-showPrimes(10);
+showPrimes(100);
 
 function showPrimes(limit) {
-  for (let number = 2; number <= limit; number++) 
-    if (isPrime(number)) console.log(number); 
+  for (let number = 2; number <= limit; number++)
+    if (isPrime(number)) console.log(number);
 }
 
 function isPrime(number) {
-  for (let factor = 2; factor < number; factor++) 
-    if (number % factor === 0) 
-      return false; 
-  
-  return true; 
+  for (let factor = 2; factor <= 9; factor++)
+    if (number % factor === 0) return false;
+
+  return true;
 }
